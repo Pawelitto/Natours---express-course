f93cd21b17fe11a14c338dbfc7bf1513af8c59e6
Paweł Pietras <pawel.pietras144@gmail.com>
2024-03-20 13:14:22 +0100

    connection mongoDB

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
-dotenv.config({ path: './config.env' });
+dotenv.config({ path: './.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<password>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+  })
+  .then(() => console.log('Connection succed'));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
